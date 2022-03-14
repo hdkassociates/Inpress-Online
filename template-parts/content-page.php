9816560c87bbaf57fe17832972f6104ac097b151
jannickathdk <71338695+jannickathdk@users.noreply.github.com>
2022-03-14 15:04:25 +0000

    Support: Header updates

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -13,8 +13,8 @@
 	
 	<header class="entry-header">
 		<?php the_post_thumbnail('hero-banner'); ?>
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
+	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 	
 
